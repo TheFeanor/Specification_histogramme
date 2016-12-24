6df10aa4df1c25914ec725ed34b94b5aceafabdd
Thomas E <teboli@enst.fr>
2016-12-24 12:03:21 +0100

    Correction de l'application du bruit uniforme à l'image

--- histogram_convol.m
+++ histogram_convol.m
@@ -21,15 +21,25 @@ H = hsGauss(L,R);
 figure;im(fx);  % ARTIFACTS IN THE SKY !!!
 x = chm_mult(xo,fx) ;figure;  im(x)
 
+h=hist(x(:),256); h=h/numel(x);
+figure;
+bar([0:1:255],h);xlim([0,255])
+
+%% gaussienne : sigma = 1
+b = 1*randn(size(x));
+xx = x + b;
+figure; im(xx); title('gauss, sigma = 1');
+
+h=hist(xx(:),256); h=h/numel(xx);
+figure;
+bar([0:1:255],h);xlim([0,255]);
+title('gauss, sigma = 1');
+
 %% gaussienne : sigma = 3
 
 b = 3*randn(size(x));
 xx = x + b;
 figure; im(xx); title('gauss, sigma = 3');
-
-h=hist(x(:),256); h=h/numel(x);
-figure;
-bar([0:1:255],h);xlim([0,255])
 
 h=hist(xx(:),256); h=h/numel(xx);
 figure;
@@ -58,9 +68,22 @@ figure;
 bar([0:1:255],h);xlim([0,255]);
 title('gauss, sigma = 10');
 
+%% porte : sigma : 1
+
+b = -1 + (2*1)*rand(size(x));
+xx = x + b;
+figure; im(xx); title('porte, sigma = 1');
+
+figure; im(rgb2gray(uint8(xx)));
+
+h=hist(xx(:),256); h=h/numel(xx);
+figure;
+bar([0:1:255],h);xlim([0,255]);
+title('porte, sigma = 1');
+
 %% porte : sigma : 3
 
-b = 3*rand(size(x));
+b = -3 + (2*3)*rand(size(x));
 xx = x + b;
 figure; im(xx); title('porte, sigma = 3');
 
@@ -71,7 +94,7 @@ title('porte, sigma = 3');
 
 %% porte : sigma = 5
 
-b = 5*rand(size(x));
+b = -5 + (2*5)*rand(size(x));
 xx = x + b;
 figure; im(xx); title('porte, sigma = 5');
 
@@ -82,7 +105,7 @@ title('porte, sigma = 5');
 
 %% gaussienne : sigma = 10
 
-b = 10*rand(size(x));
+b = -10 + (2*10)*rand(size(x));
 xx = x + b;
 figure; im(xx); title('porte, sigma = 10');
 
