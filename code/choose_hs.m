7f0cdcbb5e33c5af8acb6e7e8ab243844145d6d1
teboli <thomas.eboli12@gmail.com>
2017-02-28 16:15:35 +0100

    Choice between gaussian and laplacian shape

--- code/choose_hs.m
+++ code/choose_hs.m
@@ -1,14 +1,14 @@
 function H = choose_hs(im)
     h=hist(im(:),256); h=h/numel(im);
     H = zeros(256, 1);
-    
+
     [opt, idx] = sort(h, 'descend');
-    
+
     compt = 1;
-    dist  = 50; 
+    dist  = 50;
     former_idx = 1000*ones(10,1);
     j=1;
-    
+
     for k=1:100
         new_idx = ones(10,1)*idx(k);
         test = 1;
@@ -18,6 +18,32 @@ function H = choose_hs(im)
             end
         end
         if (k==1) || test == 1
+          % choisir la forme de l'approximation (gaussienne ou laplacienne) en fonction
+          % de l'allure du pic détecté
+          mid_val = -1;
+          if idx(k) > 30 || idx(k) < 30
+              inter_idx = (idx(k)-30):(idx(k)+30);
+          elseif idx(k) < 30
+              iter_idx = 1:(idx(k)+30);
+          else
+              inter_idx = (idx(k)-30):256
+          mid_id  = inter_idx(opt(inter_idx)==opt(k)/2);
+          if sum(mid_id > 0)
+              mid_val = opt(mid_id)
+              mid_val = min(abs(mid_val-idx(k)));
+
+          % si la valeur du milieu n'est pas dans l'intervalle d'intérêt
+          if mid_val == -1
+              type = 'gau';
+          else
+              if abs(mid_val-idx <15)
+                 type = 'lap';
+              else
+                  type = 'gau';
+              end
+          end
+
+          % trouver la meilleure position pour le pic
             ratio = opt(k)/opt(1);
             compt = compt+1;
             former_idx(j) = idx(k);
@@ -46,8 +72,8 @@ function H = choose_hs(im)
         figure;
         bar([0:1:255],H);xlim([0,255])
         end
-        
+
     end
-            
-    
+
+
 end
